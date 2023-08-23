4306b29513d61adf9580e529787251ba2a928b85
Cassandra Forestier <cassandra.forestier@hotmail.fr>
2023-08-23 14:49:26 +0200

    fixing images

--- src/page/cosplay-maker.js
+++ src/page/cosplay-maker.js
@@ -33,41 +33,41 @@ const CosplayMaker = () => {
                     className="card-frontend"
                 >
                     <div>
-                        <img src="./logo/html.png" alt="Html icon" className="html-icon" />
-                        <img src="./logo/css.png" alt="Css icon" className="css-icon" />
+                        <img src="logo/html.png" alt="Html icon" className="html-icon" />
+                        <img src="logo/css.png" alt="Css icon" className="css-icon" />
                     </div>
                     <div>
-                        <img src="./logo/react.png" alt="React icon" className="react-icon" />
-                        <img src="./logo/antdesign.png" alt="Ant Design icon" className="antd-icon" />
+                        <img src="logo/react.png" alt="React icon" className="react-icon" />
+                        <img src="logo/antdesign.png" alt="Ant Design icon" className="antd-icon" />
                     </div>
                 </Card>
                 <Card
                     title="Back-end"
                     className="card-backend"
                 >
-                    <img src="./logo/nodejs.png" alt="NodeJS icon" className="nodejs-icon" />
-                    <img src="./logo/expressjs.png" alt="Express icon" className="express-icon" />
+                    <img src="logo/nodejs.png" alt="NodeJS icon" className="nodejs-icon" />
+                    <img src="logo/expressjs.png" alt="Express icon" className="express-icon" />
 
                 </Card>
                 <Card
                     title={t("cosplaymaker.database-card")}
                     className="card-database"
                 >
-                    <img src="./logo/mongodb.png" alt="MongoDB icon" className="mongodb-icon" />
-                    <img src="./logo/mongoose.png" alt="Mongoose icon" className="mongoose-icon" />
+                    <img src="logo/mongodb.png" alt="MongoDB icon" className="mongodb-icon" />
+                    <img src="logo/mongoose.png" alt="Mongoose icon" className="mongoose-icon" />
                 </Card>
                 <Card
                     title="DevOps"
                     className="card-devops"
                 >
-                    <img src="./logo/docker.png" alt="Docker icon" className="docker-icon" />
-                    <img src="./logo/githubactions.png" alt="Github action icon" className="github-icon" />
+                    <img src="logo/docker.png" alt="Docker icon" className="docker-icon" />
+                    <img src="logo/githubactions.png" alt="Github action icon" className="github-icon" />
                 </Card>
                 <Card
                     title={t("cosplaymaker.mobile-application-card")}
                     className="card-mobile"
                 >
-                    <img src="./logo/reactnative.png" alt="React-native icon" className="react-native-icon" />
+                    <img src="logo/reactnative.png" alt="React-native icon" className="react-native-icon" />
                 </Card >
             </div >
 
@@ -75,14 +75,14 @@ const CosplayMaker = () => {
                 <div className="download-btn">
                     <Title level={5}>{t("cosplaymaker.download")}</Title>
                     <Button
-                        href="./ppt.pdf"
+                        href="ppt.pdf"
                         download="ppt.pdf"
                         className="pdf-download-link"
                     >
                         <DownloadOutlined />{t("cosplaymaker.download-powerpoint")}
                     </Button>
                     <Button
-                        href="./rapport-cosplay-maker.pdf"
+                        href="rapport-cosplay-maker.pdf"
                         download="rapport-cosplay-maker.pdf"
                         className="pdf-download-link"
                     >
@@ -91,8 +91,8 @@ const CosplayMaker = () => {
                 </div>
                 :
                 <>
-                    <embed src='./ppt.pdf' width='60%' height='600' type='application/pdf' className="ppt-display" />
-                    <embed src='./rapport-cosplay-maker.pdf' width='60%' height='600' type='application/pdf' className="pdf-display" />
+                    <embed src='ppt.pdf' width='60%' height='600' type='application/pdf' className="ppt-display" />
+                    <embed src='rapport-cosplay-maker.pdf' width='60%' height='600' type='application/pdf' className="pdf-display" />
                 </>
             }
 
